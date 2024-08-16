b1bdabb3bd3c5c70688147c8af207ee7ebee94a4
Micaela Sarotto <micaelasarotto@gmail.com>
2024-08-16 19:58:31 -0300

    div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var buttonContainer = document.getElementById('button-container');
+    buttonContainer.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
